cad3b15166ebe8cfb8a745b8fdbe02321e223223
HerveJules <hervejulesiradukunda@gmail.com>
2019-04-08 11:13:41 +0200

    hahahha

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 console.log('this is for test')
 /*
  *this file is going to be developed in testing branch
@@ -12,3 +13,8 @@ console.log('this is for test')
 
 let num = 23
 console.log(`i am ${num} years old`)
+=======
+# index.js file to replace the one from master branch
+
+
+>>>>>>> readme-edits
